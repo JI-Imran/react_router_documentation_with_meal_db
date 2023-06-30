00760fbb542bad0cc4423e32a1affef0c7b88fd5
JI-Imran <jiimran525@gmail.com>
2023-07-01 00:11:39 +0600

    react router1

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Root from './routes/root.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root/>,
   },
 ]);
 
